8555ea5b01e37187402de96ed4db200b4411a291
Your Name <antony.adriano@stundent.moringaschool.com>
2022-12-12 21:48:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
-/* Your Code Here */
+/* Your Code Here */function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}    
+//          createEmployeeRecords
+function createEmployeeRecords(arrayOfArrays){
+    return arrayOfArrays.map(array => createEmployeeRecord(array))
+}
+
+function createTimeInEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+ 
+    let [date, hour] = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+//          hoursWorkedOnDate
+function hoursWorkedOnDate(date){
+
+    let timeIn = this.timeInEvents.find(function(e){
+        return e.date === date
+    })
+
+    let timeOut = this.timeOutEvents.find(function(e){
+        return e.date === date
+    })
+
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date){
+    let wage = hoursWorkedOnDate.call(this, date) * this.payPerHour
+    return parseFloat(wage.toString())
+}
+
+//Dependent functions: findEmployeeByFirstName(collection, firstNameString)
+function findEmployeeByFirstName(srcArray, firstName){
+    //finds "Loki"
+    return srcArray.find(function(record){
+        return record.firstName === firstName
+    }
+    )
+}
+
+
+//          findEmployeeByFirstName
+function calculatePayroll(arrayOfEmployeeRecords){
+    // correctly sums the payroll burden to $11,880 when passed an array of employee records
+
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){ 
+        return memo + allWagesFor.call(rec)
+    }
+    , 0)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
